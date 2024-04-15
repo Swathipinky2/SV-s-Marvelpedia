5035c3753880982721ef3ab9d9e081144d3b150b
Swathipinky2 <swathip09876@gmail.com>
2024-04-15 14:15:19 +0530

    page creation and styles

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from './Header';
+import Section1 from './Section1';
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Section1 />
+    </div>
+  );
+}
